2013640c7be611a053c5d4bbf98bc0dcaf454350
Amina <maminan007@gmail.com>
2025-11-29 11:09:23 +0300

    add info about group

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -17,25 +17,47 @@ public class Group {
         if (memberCount < number) {
             members[memberCount] = user;
             memberCount++;
-        } else { System.out.println("группа полная"); }
+        } else {
+            System.out.println("группа полная");
+        }
     }
 
     public void removeMember(User user) {
         for (int i = 0; i < memberCount; i++) {
             if (members[i] == user) {
-                members[i] = members[memberCount - 1]; //последний теперь присоединившийся
+                members[i] = members[memberCount - 1]; // последний теперь присоединившийся
                 members[memberCount - 1] = null;
                 memberCount--;
                 break;
             }
         }
     }
-    //новый массив
+
+    // новый массив
     public User[] getMembers() {
         User[] currentMembers = new User[memberCount];
         for (int i = 0; i < memberCount; i++) {
             currentMembers[i] = members[i];
         }
         return currentMembers;
+    }
+
+    // инфа о группе
+    public class Info {
+        private StringBuilder stringBuilder;//класс для аппенда
+
+        public Info() {
+            stringBuilder = new StringBuilder();
+        }
+
+        public Info append(String str) {
+            stringBuilder.append(str);
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return stringBuilder.toString();
+        }
     }
 }
