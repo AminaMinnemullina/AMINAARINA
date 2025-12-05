ee29c947681b335b86ea7ee5b141cb4105b1a4a0
Arina <arina.klushnik@gmail.com>
2025-12-06 04:16:21 +0500

    возвращение к истокам

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,6 @@
-import java.util.List;
-import java.util.ArrayList;
 public class Main {
-    private static final List<Message> messages = new ArrayList<>();
     public static void main(String[] args) {
 
-        User[] users = new User[4];
-        users[0] = new User("Амина", "666");
-        users[1] = new User("Арина", "007");
-        users[2] = new User("Богдаша", "123");
-        users[3] = new User("Айгизя", "456");
-
-       /* sendMessage(users[0], users[1], "Привет!");
-        sendMessage(users[2], users[3], "Как дела?");
-        sendMessage(users[1], users[0], "Что задали по проге?");
-*/
-        System.out.println("Пользователи: ");
-        for (User user : users) {
-            System.out.println(user.toString());
-
-        }
     }
 }
 
