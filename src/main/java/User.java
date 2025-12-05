89bb1158d498a16adf7bb76e2d65f86f5dbee9c3
Arina <arina.klushnik@gmail.com>
2025-12-06 02:05:48 +0500

    добавление метода sendMessage в User

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,11 +1,14 @@
 public class User {
 
-    final String name;
-    final String id;
+    private String name;
+    private String id;
+    private Message[] messages;
+    private int messageCount;
 
     public User(String name, String id){
         this.name = name;
         this.id = id;
+        this.messages = new Message[1000];
     }
 
     public String getName(){
@@ -16,9 +19,30 @@ public class User {
         return id;
     }
 
-    @Override
-    public String toString() {
-        return ("Имя: " + getName() + ", ID: " + getId());
+    public int getMessageCount() {
+        return messageCount;
+    }
 
+    public void sendMessage(User toUser, String text, String date, String time){
+        Message message = new Message(this, toUser, text, time, date);
+        if (messageCount < messages.length) {
+            messages[messageCount] = message;
+            messageCount++;
+        } else {
+            System.out.println("Массив сообщение для " + name + " переполнен");
+        }
+        toUser.receiveMessage(message);
+    }
+
+    public void receiveMessage(Message message) {
+        if (messageCount < messages.length) {
+            messages[messageCount] = message;
+            messageCount++;
+        }
+    }
+
+
+    public Message[] getMessages() {
+        return messages;
     }
 }
